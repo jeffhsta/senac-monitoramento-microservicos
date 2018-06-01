ab3e94e129952b40b18c5d5aab96097ac43344ab
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2018-06-01 03:14:56 -0300

    Log client request

--- node_app/request.js
+++ node_app/request.js
@@ -1,13 +1,20 @@
 const rp = require( 'request-promise' );
+const logger = require( './logger' );
 
 const request = async nextService => {
   if ( !nextService ) { return {}; }
 
-  return rp( {
+  const startTime = Date.now();
+  const result = rp( {
     uri: `http://${nextService}/`,
     headers: { 'X-Request-Id': process.env.REQUEST_ID },
     json: true
   } );
+
+  const time = Date.now() - startTime;
+  logger.debug( { url: `http://${nextService}/`, time }, { type: 'http_client' } );
+
+  return result;
 };
 
 module.exports = request;
